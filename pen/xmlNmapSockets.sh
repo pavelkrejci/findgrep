8d5124982c8ef7ac212a208dbd22304feaa87af0
Pavel Krejci <rb@EvidenIFtools.(none)>
2024-04-12 16:32:24 +0200

    aaa

--- pen/xmlNmapSockets.sh
+++ pen/xmlNmapSockets.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+XMLS="/usr/bin/xmlstarlet"
 
 usage() {
 	BN=`basename $0`
@@ -12,13 +13,20 @@ usage() {
 	exit 2
 }
 
-XMLS="/usr/bin/xmlstarlet"
+fixXML() {
+	if grep -q -l -r "</nmaprun>" "$1"; then
+		cat "$1"
+	else
+		echo "</nmaprun>" | cat "$1" -
+	fi
+}
+
 ############################################
 # OPTIONS
 ############################################
 MODE="i"
 SORT=0
-while getopts "ic" opt; do
+while getopts "ics" opt; do
 	case "$opt" in
 		i)
 			MODE="i"
@@ -27,7 +35,7 @@ while getopts "ic" opt; do
 			MODE="c"
 			;;
 		s)
-			SORT=1
+			MODE="s"
 			;;
         \?)
             echo "Error: Invalid option: -$OPTARG" >&2
@@ -41,11 +49,13 @@ while getopts "ic" opt; do
 done
 
 shift $((OPTIND-1))
-FILE="$1"
 
+FILE="$1"
 [ ! -r "$FILE" ] && usage "Error: Cannot read file '$FILE'."
 
 [ ! -x "$XMLS" ] && usage "Error: XML parser $XMLS not found."
+
+
 
 ############################################
 # MAIN
@@ -53,10 +63,10 @@ FILE="$1"
 
 if [ "$MODE" == "i" ]; then
 	#echo "List of live IPs:"
-	$XMLS sel --recover -T -t -m "//address[../ports/port[state/@state='open']]" -v "@addr" -n $FILE
+	fixXML $FILE | $XMLS sel --recover -T -t -m "//address[../ports/port[state/@state='open']]" -v "@addr" -n
 elif [ "$MODE" == "c" ]; then
 	#$XMLS sel --recover -T -t -m "//host" -v "address/@addr" -o "," -m "ports/port" -v "@portid" -n input.xml > output.csv
-	$XMLS sel --recover -T -t -m "//host/address[../ports/port[state/@state='open']]" -v "@addr" -m "../ports/port[state/@state='open']" -o "," -v "@portid" -n $FILE
+	fixXML $FILE | $XMLS sel --recover -T -t -m "//host/address[../ports/port[state/@state='open']]" -v "@addr" -m "../ports/port[state/@state='open']" -o "," -v "@portid" -n
 #	xmlstarlet sel -t -m "//address@addr" -s "substring-before(., '.')" -s "substring-before(substring-after(., '.'), '.')" -s "substring-before(substring-after(substring-after(., '.'), '.'), '.')" -s "substring-after(substring-after(substring-after(., '.'), '.'), '.')" -v . -n $FILE
 
 	#$XMLS sel --recover -T -t -m "//address[../ports/port[state/@state='open']]" -v "@addr" -o "," -m "ports/port" -v "@portid" -n $FILE
@@ -67,7 +77,42 @@ elif [ "$MODE" == "c" ]; then
 	#	$XMLS sel --recover -T -t -m "//address[@addr='$a']" -m "../ports/port[state/@state='open']" -v "@portid" -n $FILE
 	#done
 
-
+elif [ "$MODE" == "s" ]; then
+	#/usr/bin/xsltproc <(cat <<'EOF'
+	fixXML $FILE | $XMLS tr --recover <(cat <<'EOF'
+<?xml version="1.0"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:exslt="http://exslt.org/common" version="1.0" extension-element-prefixes="exslt">
+	<xsl:output omit-xml-declaration="yes" indent="no" method="text"/>
+	<xsl:template match="/">
+		<xsl:for-each select="//host/address">
+			<xsl:sort select="substring-before(@addr, '.')" data-type="number"/>
+			<xsl:sort select="substring-before(substring-after(@addr, '.'), '.')" data-type="number"/>
+			<xsl:sort select="substring-before(substring-after(substring-after(@addr, '.'), '.'), '.')" data-type="number"/>
+			<xsl:sort select="substring-after(substring-after(substring-after(@addr, '.'), '.'), '.')" data-type="number"/>
+			<!--			<xsl:call-template name="value-of-template">
+				<xsl:with-param name="select" select="@addr"/>
+			</xsl:call-template> -->
+			<xsl:variable name="ip" select="@addr"/>
+			<xsl:for-each select="../ports/port[state/@state='open']">
+				<xsl:value-of select="$ip"/>
+				<xsl:text>:</xsl:text>
+				<xsl:value-of select="@portid"/>
+				<xsl:text>&#10;</xsl:text>
+			</xsl:for-each>
+		</xsl:for-each>
+	</xsl:template>
+	<!--
+	<xsl:template name="value-of-template">
+		<xsl:param name="select"/>
+		<xsl:value-of select="$select"/>
+		<xsl:for-each select="exslt:node-set($select)[position()&gt;1]">
+			<xsl:value-of select="'&#10;'"/>
+			<xsl:value-of select="."/>
+		</xsl:for-each>
+	</xsl:template> -->
+</xsl:stylesheet>
+EOF
+)
 fi
 
 exit 0
