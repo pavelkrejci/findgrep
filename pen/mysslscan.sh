5f909a8e2c40c34033b756da1335c8e35f52852f
PENguin Deployment <pentest-user4@atos.net>
2024-05-20 15:37:13 +0200

    aaa

--- pen/mysslscan.sh
+++ pen/mysslscan.sh
@@ -8,6 +8,7 @@ usage() {
 	echo "Usage: $BN <options> <IP socket> | -f <IP sockets list file>"
 	echo "- uses sslscan to scan single IP socket, or a file with IP sockets, one per line"
 	echo "<options>:"
+	echo "-n <SNI name> = use different SNI (Virtual Host) name"
 	echo "-e <path to sslscan binary>"
 	echo "-s sleep in msec, pause between connection request. default is 100msec"
 	exit 2
@@ -18,11 +19,14 @@ usage() {
 ############################################
 MODE=""
 SLEEP="--sleep=100"
-while getopts "f:s:e:" opt; do
+while getopts "f:s:e:n:" opt; do
 	case "$opt" in
 		f)
 			MODE="f"
 			FILE=$OPTARG
+			;;
+		n)
+			SNI="--sni-name=$OPTARG"
 			;;
 		s)
 			SLEEP="--sleep=$OPTARG"
@@ -48,12 +52,16 @@ if [ "$MODE" == "f" ]; then
 	[ ! -r "$FILE" ] && usage "Error: Cannot read file '$FILE'."
 	#broken PIPE error (exit code 141) must be ignored, otherwise sslscan fails on non HTTPS ports
 	OUTXML="${FILE%.*}.xml"
-	trap '' PIPE; $SSLSCAN --show-certificate --connect-timeout=5 --ipv4 --verbose $SLEEP --xml=$OUTXML --targets=$FILE
+	CMD="$SSLSCAN --show-certificate --connect-timeout=5 --ipv4 --verbose $SLEEP $SNI --xml=$OUTXML --targets=$FILE"
+	echo $CMD
+	trap '' PIPE; $CMD
 	echo "Result file output: $OUTXML"
 else
 	[ -z "$1" ] && usage "Error: IP socket not specified."
 	OUTXML="out-$1.xml"
-	trap '' PIPE; $SSLSCAN --show-certificate --connect-timeout=5 --ipv4 --verbose --xml=$OUTXML "$1"
+	CMD="$SSLSCAN --show-certificate --connect-timeout=5 --ipv4 --verbose $SNI --xml=$OUTXML $1"
+	echo $CMD
+	trap '' PIPE; $CMD
 	echo "Result file output: $OUTXML"
 fi
 
