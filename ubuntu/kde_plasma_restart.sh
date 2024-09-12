23721a50a08748127859d74036b9a3a3914f21a8
ucednik <pavel@krejci.tel>
2024-09-12 12:48:07 +0200

    aaa

--- ubuntu/kde_plasma_restart.sh
+++ ubuntu/kde_plasma_restart.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 #
 #
-systemctl --user status -l --no-pager plasma-plasmashell.service
 
-if pgrep plasmashell; then
-	echo -n "The shell is still running. Do you really want to restart the service plasma-plasmashell.service? [y/n]:"
-	read a
-	[ "$a" != "y" ] && exit 2
+while getopts "y" opt; do
+	case "$opt" in
+		y)
+			MODE=y
+			;;
+        \?)
+            echo "Error: Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Error: Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ "$MODE" != "y" ]; then
+	systemctl --user status -l --no-pager plasma-plasmashell.service
+
+	if pgrep plasmashell; then
+		echo -n "The shell is still running. Do you really want to restart the service plasma-plasmashell.service? [y/n]:"
+		read a
+		[ "$a" != "y" ] && exit 2
+	fi
 fi
 
 echo go to restart ...
