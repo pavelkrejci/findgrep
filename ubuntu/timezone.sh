0fcd831ab546fb45e3ae3fb184d1c62750654acc
ucednik <ucednik@centrum.cz>
2025-05-05 16:30:34 +0200

    a

--- ubuntu/timezone.sh
+++ ubuntu/timezone.sh
@@ -6,6 +6,5 @@ sudo apt-get update && sudo apt-get install -y tzdata
 # Set timezone 
 TZ=${1:-Europe/Prague}
 
-echo $TZ
-sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && sudo echo $TZ > /etc/timezone
+sudo ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && sudo echo $TZ > /etc/timezone && echo "Timezone $TZ has been set."
 
