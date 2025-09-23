2f618c326103f1db47e2726b27985277ef132c62
ucednik <pavel@krejci.tel>
2025-09-23 10:55:04 +0200

    update

--- snip.sh
+++ snip.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+FILE=`mktemp -p /tmp snip.sh.XXXXXXXX.png`
+#/usr/bin/maim -f png -s | /usr/bin/xclip -selection clipboard -t image/png
+/usr/bin/maim -f png -s $FILE && spectacle -E $FILE
 
-/usr/bin/maim -f png -s | /usr/bin/xclip -selection clipboard -t image/png
+rm -f $FILE
